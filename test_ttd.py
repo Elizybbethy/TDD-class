739f7f4e3f0c0709327535e512a44b0844498516
elizy <elizybbethy@gmail.com>
2023-10-15 12:20:24 +0300

    Cycle 3

--- test_ttd.py
+++ test_ttd.py
@@ -1,10 +1,14 @@
 def multiply(a, b):
     return a*b
 
-# def test_multiply():
-#     result = multiply(1,1)
-#     assert result ==1
+def test_multiply():
+    result = multiply(1,1)
+    assert result ==1
 
 def test_multiply():
     result2 = multiply(2,2)
     assert result2 ==4
+    
+def test_multiply():
+    result3 = multiply(3,3)
+    assert result3 ==9
