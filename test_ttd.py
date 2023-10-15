f468a483a8e35d734c213e7f55b208586c8cd8b3
elizy <elizybbethy@gmail.com>
2023-10-15 12:28:34 +0300

    Cycle 5

--- test_ttd.py
+++ test_ttd.py
@@ -1,18 +1,27 @@
 def multiply(a, b):
-    return a*b
+    return a * b
+
 
 def test_multiply():
-    result = multiply(1,1)
-    assert result ==1
+    result = multiply(1, 1)
+    assert result == 1
+
 
 def test_multiply():
-    result2 = multiply(2,2)
-    assert result2 ==4
-    
+    result2 = multiply(2, 2)
+    assert result2 == 4
+
+
 def test_multiply():
-    result3 = multiply(3,3)
-    assert result3 ==9
-    
+    result3 = multiply(3, 3)
+    assert result3 == 9
+
+
 def test_multiply():
-    result4 = multiply(4,4)
-    assert result4 ==16
+    result4 = multiply(4, 4)
+    assert result4 == 16
+
+
+def test_multiply():
+    result5 = multiply(23, 45)
+    assert result5 == 23 * 45
