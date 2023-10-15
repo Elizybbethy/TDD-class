f694454ae730daf9c67f4102fdc2b418ac0b3eb6
elizy <elizybbethy@gmail.com>
2023-10-15 12:16:59 +0300

    Cycle 2

--- test_ttd.py
+++ test_ttd.py
@@ -1,6 +1,10 @@
 def multiply(a, b):
     return a*b
 
+# def test_multiply():
+#     result = multiply(1,1)
+#     assert result ==1
+
 def test_multiply():
-    result = multiply(1,1)
-    assert result ==1
+    result2 = multiply(2,2)
+    assert result2 ==4
